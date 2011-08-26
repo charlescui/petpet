03ea4f2f5dfdd26bf80b5a4ab1e85b5e2904801f
cuizheng <zheng.cuizh@gmail.com>
2011-08-26 10:19:10 +0800

    modified

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petpet::Application.config.secret_token = '09693dcebe8ce835b1a787ebdb094a29664e92abf8a15976d113dae14c16cb4017132aa3f4209cda039f57efb7a2187f583f021783568de8465a2f7690f7ce50'
